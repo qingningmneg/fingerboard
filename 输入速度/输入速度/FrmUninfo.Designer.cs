dfe4f900a6353d4f3b9dc11d973f35c0c3d60ece
ningmeng <3571333849@qq.com>
2023-03-13 15:17:48 +0800

    1
    Signed-off-by: ningmeng <3571333849@qq.com>

---
+++ 输入速度/输入速度/FrmUninfo.Designer.cs
@@ -0,0 +1,247 @@
+﻿
+namespace 输入速度
+{
+    partial class FrmUninfo : DevExpress.XtraEditors.XtraForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Totalinput = new DevExpress.XtraEditors.LabelControl();
+            this.Inputspeed = new DevExpress.XtraEditors.LabelControl();
+            this.Totalinputs = new DevExpress.XtraEditors.LabelControl();
+            this.Inputspeeds = new DevExpress.XtraEditors.LabelControl();
+            this.calculation = new DevExpress.XtraEditors.SimpleButton();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.timers = new System.Windows.Forms.Timer(this.components);
+            this.Time = new DevExpress.XtraEditors.LabelControl();
+            this.Setthetop = new DevExpress.XtraEditors.SimpleButton();
+            this.transparency = new DevExpress.XtraEditors.SimpleButton();
+            this.toolbarFormManager1 = new DevExpress.XtraBars.ToolbarForm.ToolbarFormManager(this.components);
+            this.barDockControlTop = new DevExpress.XtraBars.BarDockControl();
+            this.barDockControlBottom = new DevExpress.XtraBars.BarDockControl();
+            this.barDockControlLeft = new DevExpress.XtraBars.BarDockControl();
+            this.barDockControlRight = new DevExpress.XtraBars.BarDockControl();
+            this.timess = new DevExpress.XtraEditors.LabelControl();
+            this.TimeButton = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.toolbarFormManager1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Totalinput
+            // 
+            this.Totalinput.Location = new System.Drawing.Point(6, 16);
+            this.Totalinput.Margin = new System.Windows.Forms.Padding(2);
+            this.Totalinput.Name = "Totalinput";
+            this.Totalinput.Size = new System.Drawing.Size(52, 14);
+            this.Totalinput.TabIndex = 0;
+            this.Totalinput.Text = "输入总数:";
+            // 
+            // Inputspeed
+            // 
+            this.Inputspeed.Location = new System.Drawing.Point(144, 16);
+            this.Inputspeed.Margin = new System.Windows.Forms.Padding(2);
+            this.Inputspeed.Name = "Inputspeed";
+            this.Inputspeed.Size = new System.Drawing.Size(52, 14);
+            this.Inputspeed.TabIndex = 1;
+            this.Inputspeed.Text = "输入速度:";
+            // 
+            // Totalinputs
+            // 
+            this.Totalinputs.Location = new System.Drawing.Point(76, 16);
+            this.Totalinputs.Margin = new System.Windows.Forms.Padding(2);
+            this.Totalinputs.Name = "Totalinputs";
+            this.Totalinputs.Size = new System.Drawing.Size(57, 14);
+            this.Totalinputs.TabIndex = 2;
+            this.Totalinputs.Text = "emmmmm";
+            // 
+            // Inputspeeds
+            // 
+            this.Inputspeeds.Location = new System.Drawing.Point(214, 16);
+            this.Inputspeeds.Margin = new System.Windows.Forms.Padding(2);
+            this.Inputspeeds.Name = "Inputspeeds";
+            this.Inputspeeds.Size = new System.Drawing.Size(57, 14);
+            this.Inputspeeds.TabIndex = 3;
+            this.Inputspeeds.Text = "emmmmm";
+            // 
+            // calculation
+            // 
+            this.calculation.Location = new System.Drawing.Point(517, 10);
+            this.calculation.Margin = new System.Windows.Forms.Padding(2);
+            this.calculation.Name = "calculation";
+            this.calculation.Size = new System.Drawing.Size(59, 27);
+            this.calculation.TabIndex = 4;
+            this.calculation.Text = "开始计算";
+            this.calculation.Click += new System.EventHandler(this.calculation_Click);
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 1;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // timers
+            // 
+            this.timers.Enabled = true;
+            this.timers.Interval = 1000;
+            this.timers.Tick += new System.EventHandler(this.timers_Tick);
+            // 
+            // Time
+            // 
+            this.Time.Location = new System.Drawing.Point(360, 16);
+            this.Time.Name = "Time";
+            this.Time.Size = new System.Drawing.Size(24, 14);
+            this.Time.TabIndex = 5;
+            this.Time.Text = "用时";
+            // 
+            // Setthetop
+            // 
+            this.Setthetop.Location = new System.Drawing.Point(638, 10);
+            this.Setthetop.Margin = new System.Windows.Forms.Padding(2);
+            this.Setthetop.Name = "Setthetop";
+            this.Setthetop.Size = new System.Drawing.Size(54, 27);
+            this.Setthetop.TabIndex = 6;
+            this.Setthetop.Text = "窗体设顶";
+            this.Setthetop.Click += new System.EventHandler(this.Setthetop_Click);
+            // 
+            // transparency
+            // 
+            this.transparency.Location = new System.Drawing.Point(580, 10);
+            this.transparency.Margin = new System.Windows.Forms.Padding(2);
+            this.transparency.Name = "transparency";
+            this.transparency.Size = new System.Drawing.Size(54, 27);
+            this.transparency.TabIndex = 7;
+            this.transparency.Text = "透明度";
+            this.transparency.Click += new System.EventHandler(this.transparency_Click);
+            // 
+            // toolbarFormManager1
+            // 
+            this.toolbarFormManager1.DockControls.Add(this.barDockControlTop);
+            this.toolbarFormManager1.DockControls.Add(this.barDockControlBottom);
+            this.toolbarFormManager1.DockControls.Add(this.barDockControlLeft);
+            this.toolbarFormManager1.DockControls.Add(this.barDockControlRight);
+            this.toolbarFormManager1.Form = this;
+            // 
+            // barDockControlTop
+            // 
+            this.barDockControlTop.CausesValidation = false;
+            this.barDockControlTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.barDockControlTop.Location = new System.Drawing.Point(0, 0);
+            this.barDockControlTop.Manager = this.toolbarFormManager1;
+            this.barDockControlTop.Size = new System.Drawing.Size(703, 0);
+            // 
+            // barDockControlBottom
+            // 
+            this.barDockControlBottom.CausesValidation = false;
+            this.barDockControlBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.barDockControlBottom.Location = new System.Drawing.Point(0, 58);
+            this.barDockControlBottom.Manager = this.toolbarFormManager1;
+            this.barDockControlBottom.Size = new System.Drawing.Size(703, 0);
+            // 
+            // barDockControlLeft
+            // 
+            this.barDockControlLeft.CausesValidation = false;
+            this.barDockControlLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this.barDockControlLeft.Location = new System.Drawing.Point(0, 0);
+            this.barDockControlLeft.Manager = this.toolbarFormManager1;
+            this.barDockControlLeft.Size = new System.Drawing.Size(0, 58);
+            // 
+            // barDockControlRight
+            // 
+            this.barDockControlRight.CausesValidation = false;
+            this.barDockControlRight.Dock = System.Windows.Forms.DockStyle.Right;
+            this.barDockControlRight.Location = new System.Drawing.Point(703, 0);
+            this.barDockControlRight.Manager = this.toolbarFormManager1;
+            this.barDockControlRight.Size = new System.Drawing.Size(0, 58);
+            // 
+            // timess
+            // 
+            this.timess.Location = new System.Drawing.Point(305, 16);
+            this.timess.Name = "timess";
+            this.timess.Size = new System.Drawing.Size(52, 14);
+            this.timess.TabIndex = 12;
+            this.timess.Text = "使用时间:";
+            // 
+            // TimeButton
+            // 
+            this.TimeButton.Location = new System.Drawing.Point(422, 11);
+            this.TimeButton.Margin = new System.Windows.Forms.Padding(2);
+            this.TimeButton.Name = "TimeButton";
+            this.TimeButton.Size = new System.Drawing.Size(91, 27);
+            this.TimeButton.TabIndex = 13;
+            this.TimeButton.Text = "目前状态(开始)";
+            this.TimeButton.Click += new System.EventHandler(this.TimeButton_Click);
+            // 
+            // FrmUninfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(703, 58);
+            this.Controls.Add(this.TimeButton);
+            this.Controls.Add(this.timess);
+            this.Controls.Add(this.transparency);
+            this.Controls.Add(this.Setthetop);
+            this.Controls.Add(this.Time);
+            this.Controls.Add(this.calculation);
+            this.Controls.Add(this.Inputspeeds);
+            this.Controls.Add(this.Totalinputs);
+            this.Controls.Add(this.Inputspeed);
+            this.Controls.Add(this.Totalinput);
+            this.Controls.Add(this.barDockControlLeft);
+            this.Controls.Add(this.barDockControlRight);
+            this.Controls.Add(this.barDockControlBottom);
+            this.Controls.Add(this.barDockControlTop);
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "FrmUninfo";
+            this.Text = "柠檬出品(测试打字速度)";
+            this.Load += new System.EventHandler(this.FrmUninfo_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.toolbarFormManager1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.LabelControl Totalinput;
+        private DevExpress.XtraEditors.LabelControl Inputspeed;
+        private DevExpress.XtraEditors.LabelControl Totalinputs;
+        private DevExpress.XtraEditors.LabelControl Inputspeeds;
+        private DevExpress.XtraEditors.SimpleButton calculation;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Timer timers;
+        private DevExpress.XtraEditors.LabelControl Time;
+        private DevExpress.XtraEditors.SimpleButton Setthetop;
+        private DevExpress.XtraEditors.SimpleButton transparency;
+        private DevExpress.XtraBars.ToolbarForm.ToolbarFormManager toolbarFormManager1;
+        private DevExpress.XtraBars.BarDockControl barDockControlTop;
+        private DevExpress.XtraBars.BarDockControl barDockControlBottom;
+        private DevExpress.XtraBars.BarDockControl barDockControlLeft;
+        private DevExpress.XtraBars.BarDockControl barDockControlRight;
+        private DevExpress.XtraEditors.LabelControl timess;
+        private DevExpress.XtraEditors.SimpleButton TimeButton;
+    }
+}
---
+++ 输入速度/输入速度/FrmUninfo.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 输入速度
+{
+    public partial class FrmUninfo : DevExpress.XtraEditors.XtraForm
+    {
+        #region 键盘钩子
+        /// <summary>
+        /// 键盘钩子
+        /// </summary>
+        static int hKeyboardHook = 0; //键盘钩子句柄
+        public const int WH_KEYBOARD_LL = 13; //处理键盘消息
+        private const int WM_KEYDOWN = 0x100;//键盘按下
+        HookProc KeyboardHookProcedure;//声明键盘钩子事件类型.
+
+        /// <summary>
+        /// 声明键盘钩子的封送结构类型
+        /// </summary>
+        [StructLayout(LayoutKind.Sequential)]
+        public class KeyboardHookStruct
+        {
+            /// <summary>
+            /// 表示一个在1到254间的虚似键盘码
+            /// </summary>
+            public int vkCode;
+        }
+        /// <summary>
+        /// 装置钩子的函数
+        /// </summary>
+        /// <param name="idHook"></param>
+        /// <param name="lpfn"></param>
+        /// <param name="hInstance"></param>
+        /// <param name="threadId"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern int SetWindowsHookEx(int idHook, HookProc lpfn, IntPtr hInstance, int threadId);
+
+        /// <summary>
+        /// 卸下钩子的函数
+        /// </summary>
+        /// <param name="idHook"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern bool UnhookWindowsHookEx(int idHook);
+
+        #region 下一个钩挂的函数
+        /// <summary>
+        /// 下一个钩挂的函数
+        /// </summary>
+        /// <param name="idHook"></param>
+        /// <param name="nCode"></param>
+        /// <param name="wParam"></param>
+        /// <param name="lParam"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern int CallNextHookEx(int idHook, int nCode, Int32 wParam, IntPtr lParam);
+        [DllImport("user32")]
+        public static extern int ToAscii(int uVirtKey, int uScanCode, byte[] lpbKeyState, byte[] lpwTransKey, int fuState);
+        [DllImport("user32")]
+        public static extern int GetKeyboardState(byte[] pbKeyState);
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+        public delegate int HookProc(int nCode, Int32 wParam, IntPtr lParam);
+        #endregion
+
+        /// <summary>
+        /// 先前按下的键
+        /// </summary>
+        public List<Keys> preKeys = new List<Keys>();
+        int b;
+
+        /// <summary>
+        /// 析构函数
+        /// </summary>
+        public void Start()
+        {
+            //安装键盘钩子
+            if (hKeyboardHook == 0)
+            {
+                KeyboardHookProcedure = new HookProc(KeyboardHookProc);
+                Process curProcess = Process.GetCurrentProcess();//获取新的 Process 组件并将其与当前活动的进程关联
+                ProcessModule curModule = curProcess.MainModule;
+                hKeyboardHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardHookProcedure, GetModuleHandle(curModule.ModuleName), 0);
+                if (hKeyboardHook == 0)
+                {
+                    Stop();
+                    throw new Exception("SetWindowsHookEx ist failed.");
+                }
+                b = 0;
+            }
+        }
+        public void Stop()
+        {
+            bool retKeyboard = true;
+            if (hKeyboardHook != 0)
+            {
+                retKeyboard = UnhookWindowsHookEx(hKeyboardHook);
+                hKeyboardHook = 0;
+            }
+            //如果卸下钩子失败
+            if (!(retKeyboard)) throw new Exception("UnhookWindowsHookEx failed.");
+        }
+        int num = 0;//记录次数
+        private int KeyboardHookProc(int nCode, Int32 wParam, IntPtr lParam)
+        {
+            if (i == true)
+            {
+                if (wParam == WM_KEYDOWN)
+                {
+                    KeyboardHookStruct MyKeyboardHookStruct = (KeyboardHookStruct)Marshal.PtrToStructure(lParam, typeof(KeyboardHookStruct));
+                    Keys keyData = (Keys)MyKeyboardHookStruct.vkCode;
+                    preKeys.Add(keyData);
+                    num++;
+                    Totalinputs.Text = Convert.ToString(num);
+                    b = 1;
+                }
+            }
+            return CallNextHookEx(hKeyboardHook, nCode, wParam, lParam);
+        }
+        #endregion
+
+        #region 函数
+        /// <summary>
+        /// 函数
+        /// </summary>
+        float nums = 0;
+        int numss = 0;
+        bool i = false;
+        bool l = false;
+        #endregion
+
+        public FrmUninfo()
+        {
+            InitializeComponent();
+            AutoScaleMode = AutoScaleMode.None;//不能缩放
+            FormBorderStyle = FormBorderStyle.FixedSingle;//不能拉边框
+            MaximizeBox = false;//禁止最大化按钮
+            //柠檬出品
+            StartPosition = FormStartPosition.CenterScreen;//起始位置居中
+        }
+
+        /// <summary>
+        /// 窗体加载事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FrmUninfo_Load(object sender, EventArgs e)
+        {
+            this.TopMost = true;
+            l = false;
+            Setthetop.Text = "取消设顶";
+            this.Opacity = 0.7;
+            Start();
+        }
+
+        /// <summary>
+        /// 开始计算按钮
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void calculation_Click(object sender, EventArgs e)
+        {
+            if (i == true)
+            {
+                Start();
+            }
+            if (calculation.Text == "开始计算")
+            {
+                TimeButton.Text = "目前状态(开始)";
+                i = true;
+                calculation.Text = "结束计算";
+                num = 0;
+                nums = 0;
+                Totalinputs.Text = num.ToString();
+                Inputspeeds.Text = "0" + "/m";
+                Time.Text = nums.ToString() + "/s";
+            }
+            else
+            {
+                TimeButton.Text = "目前状态(开始)";
+                i = false;
+                calculation.Text = "开始计算";
+                num = 0;
+                nums = 0;
+                Totalinputs.Text = num.ToString();
+                Inputspeeds.Text = "0" + "/m";
+                Time.Text = nums.ToString() + "/s";
+            }
+        }
+
+        /// <summary>
+        /// 时间事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (i == true)
+            {
+                CreateParams cp = base.CreateParams;
+                Totalinputs.Text = num.ToString();
+            }
+        }
+
+        /// <summary>
+        /// 时间事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timers_Tick(object sender, EventArgs e)
+        {
+            if (i == true)
+            {
+                nums += 1;
+                var a = num / nums * 60;
+                Time.Text = nums.ToString() + "/s";
+                Inputspeeds.Text = a.ToString() + "/m";
+            }
+        }
+
+        /// <summary>
+        /// 窗体设顶
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Setthetop_Click(object sender, EventArgs e)
+        {
+            if (l == true)
+            {
+                this.TopMost = true;
+                l = false;
+                Setthetop.Text = "取消设顶";
+            }
+            else
+            {
+                this.TopMost = false;
+                l = true;
+                Setthetop.Text = "窗体设顶";
+            }
+        }
+
+        /// <summary>
+        /// 透明度
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void transparency_Click(object sender, EventArgs e)
+        {
+            if (this.Opacity == 0.7)
+            {
+                this.Opacity = 0.8;
+            }
+            else
+            if (this.Opacity == 0.8)
+            {
+                this.Opacity = 0.9;
+            }
+            else
+            if (this.Opacity == 0.9)
+            {
+                this.Opacity = 1;
+            }
+            else
+            if (this.Opacity == 1)
+            {
+                this.Opacity = 0.7;
+            }
+        }
+
+        private void TimeButton_Click(object sender, EventArgs e)
+        {
+            if (calculation.Text == "结束计算")
+            {
+                if (TimeButton.Text == "目前状态(开始)")
+                {
+                    i = false;
+                    TimeButton.Text = "目前状态(暂停)";
+                }
+                else
+                {
+                    i = true;
+                    TimeButton.Text = "目前状态(开始)";
+                }
+            }
+        }
+    }
+}
